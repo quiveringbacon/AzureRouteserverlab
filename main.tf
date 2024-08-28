eca5a08b97b355ee06994077d93db617c6205d98
quiveringbacon <128983862+quiveringbacon@users.noreply.github.com>
2024-08-28 08:58:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -292,21 +292,21 @@ resource "azurerm_virtual_network" "onprem-vnet" {
   name                = "onprem-vnet"
   resource_group_name = azurerm_resource_group.RG.name
   subnet {
-    address_prefixes     = "192.168.0.0/24"
+    address_prefixes     = ["192.168.0.0/24"]
     name                 = "default"
     security_group = azurerm_network_security_group.onpremvnetNSG.id
   }
   subnet {
-    address_prefixes     = "192.168.1.0/24"
+    address_prefixes     = ["192.168.1.0/24"]
     name                 = "GatewaySubnet" 
   }
   subnet {
-    address_prefixes     = "192.168.2.0/24"
+    address_prefixes     = ["192.168.2.0/24"]
     name                 = "outside"
     security_group =  azurerm_network_security_group.asavsshnsg.id
   }
   subnet {
-    address_prefixes     = "192.168.3.0/24"
+    address_prefixes     = ["192.168.3.0/24"]
     name                 = "inside" 
     security_group = azurerm_network_security_group.asavnsg.id
   }
